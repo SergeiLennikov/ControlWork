bbf8855ce1ae122f6458107a02b9369d8b4621e4
Sergei Lennikov <lennikov99@list.ru>
2024-05-04 16:44:53 +0700

    Рабочий Код!

--- work1/Program.cs
+++ work1/Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿using System.Linq.Expressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество элементов массива: ");
+        int elementsCount = int.Parse(Console.ReadLine());
+        string[] myArray = new string[elementsCount];
+
+        void array(string[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"\nВведите элемент массива под индексом {i + 1}:");
+                myArray[i] = Console.ReadLine();
+            }
+        }
+        string[] symbol(string[] myArray)
+        {
+            int n = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i].Length <= 3)
+                {
+                    n++;
+                }
+            }
+            string[] sim = new string[n];
+            int k = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i].Length <= 3)
+                {
+                    sim[k] = myArray[i];
+                    k++;
+                }
+            }
+            return sim;
+
+        }
+        void PrintArray(string[] myArray)
+        {
+            Console.Write("[");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"{myArray[i]}");
+            }
+            Console.Write("]");
+        }
+        array(myArray);
+        PrintArray(symbol(myArray));
+    }
+}
--- work1/obj/Debug/net8.0/work1.AssemblyInfo.cs
+++ work1/obj/Debug/net8.0/work1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("work1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da4637564e478de236a92214a41ff59a9f5d4a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("work1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("work1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
